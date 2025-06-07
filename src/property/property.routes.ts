b09e152343c89655bb37e636c2112666dc563396
nikrr4000 <romanovnr20@gmail.com>
2025-06-07 08:07:30 +0300

    fix: add auth middleware to property routes

--- src/property/property.routes.ts
+++ src/property/property.routes.ts
@@ -1,4 +1,5 @@
 import express, { RequestHandler } from "express";
+import { authMiddleware } from "src/middleware/auth.middleware";
 import { uploadMiddleware } from "../middleware/uploade.middleware";
 import { propertyController } from "./property.controller";
 
@@ -25,7 +26,7 @@ router.get(
 );
 
 router.post("/", uploadMiddleware, asyncHandler(propertyController.createProperty));
-router.get("/moderation", asyncHandler(propertyController.listForModeration));
-router.patch("/:id/moderation", asyncHandler(propertyController.updateModerationStatus));
+router.get("/moderation", authMiddleware(), asyncHandler(propertyController.listForModeration));
+router.patch("/:id/moderation", authMiddleware(), asyncHandler(propertyController.updateModerationStatus));
 
 export default router;
